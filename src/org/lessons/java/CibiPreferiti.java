3adc564db3f350f24405f3763f263acae9a30b6d
donatomontrone <donato.montrone@outlook.com>
2023-05-04 16:53:47 +0200

    fix half element print in array

--- src/org/lessons/java/CibiPreferiti.java
+++ src/org/lessons/java/CibiPreferiti.java
@@ -5,22 +5,22 @@ public class CibiPreferiti {
 	public static void main(String[] args) {
 		
 		
-		String[] favoriteFoods = {"Pizza", "Pollo", "Pasta", "Gelato", "Hamburger", "Patatine", "Verdura" };
+		String[] favoriteFoods = {"Pizza", "Pollo", "Pasta", "Gelato", "Hamburger", "Patatine", "Verdura", "Broccoli" };
 		
 		int foodsLnt = favoriteFoods.length;
 		
 		System.out.println("Nella classifica ho inserito " + foodsLnt + " piatti.");
 		System.out.println("Questo è il mio piatto top: " + favoriteFoods[0]);
-		System.out.println("Questo è il mio piatto che preferisco meno: " + favoriteFoods[foodsLnt - 1]);
+		System.out.println("Questo è il piatto che preferisco meno: " + favoriteFoods[foodsLnt - 1]);
+		
+		int halfIndex = foodsLnt / 2;
+		String result = favoriteFoods[halfIndex];
+		System.out.println("Questo è il piatto di mezza classifica: " + result);
 		
 		if (foodsLnt % 2 == 0) {
 			
-			String result = favoriteFoods[(foodsLnt - 1) / 2];
-			System.out.println("Questo è il mio piatto di mezza classifica: " + result);
-		} else {
-			String result = favoriteFoods[foodsLnt / 2];
-			System.out.println("Questo è il mio piatto di mezza classifica: " + result);
-		}
-		
+			String result2 = favoriteFoods[halfIndex - 1];
+			System.out.println("Questo è il piatto di mezza classifica: " + result2);
+		}	
 	}
 }

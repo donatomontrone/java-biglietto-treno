d48082ac15c9a5d9fc7c64217926010fdaed5d13
donatomontrone <donato.montrone@outlook.com>
2023-05-04 14:50:33 +0200

    first commit

---
+++ src/org/lessons/java/CalcolaBiglietto.java
@@ -0,0 +1,26 @@
+package org.lessons.java;
+
+import java.util.Scanner;
+
+public class CalcolaBiglietto {
+
+	 public static void main(String[] args) {
+		Scanner in = new Scanner(System.in);
+		
+		System.out.println("Inserisci il numero di Km che vuoi percorrere");
+		int userKm = in.nextInt();
+		
+		System.out.println("Bene, adesso inserisci la tua età");
+		
+		int userAge = in.nextInt();	
+		
+		in.close();
+		
+		float ticketPrice = 0.21f;
+		
+		if (userAge > ) {
+			
+		}
+		
+	}
+}
